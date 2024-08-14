9b0488739fe0018965c66d6d701b14ad2e40765d
aeiyansiddiqiayo <aeiyan@uoguelph.ca>
2024-08-14 13:47:02 -0400

    works properly now

--- theRag.py
+++ theRag.py
@@ -1,16 +1,21 @@
 # trying to input a suggestion
+#Converge-Threat-Intel-Report-2024-MAY.pdf
+#Converge-Threat-Intel-Report-2024-APR.pdf"
+#fakeReport2.pdf
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from transformers.utils import logging
+logging.set_verbosity_error()
+
 import re
-import fitz  # PyMuPDF
+import fitz 
 import json 
-import os
 import gc
 import numpy as np
 import pandas as pd
+from sentence_transformers import util, SentenceTransformer
 
-
-#from testingRagPart2 import askingLlamaForAdvice
 
 torch.cuda.empty_cache()
 
@@ -71,9 +76,6 @@ def extract_cves_regex(text):
 
 cves = extract_cves_regex(llm_output)
 
-#for cve in cves:
-#    print(cve)
-
 # Format CVE numbers for database lookup
 def extract_cve_numbers(cve_string):
     pattern = r'CVE-(\d{4})-(\d{4,7})'
@@ -92,134 +94,63 @@ def format_second_set(second_set):
         return second_set[:2] + 'xxx'
     
 
-def askingLlamaForAdvice(cveDesp):
-    ##print(cveDesp)
-    import torch
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+def asking_llama_for_advice(cveDesp: str) -> str:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    #####testing stuff July 17th
-    #user_input_csv = "testingAll.csv"
-    user_input_csv = "CVE2024.csv"
+        csv_path = 'test6.csv'
 
-    text_chunks_and_embedding_df = pd.read_csv(user_input_csv)
+        chunk_embeddings_df = pd.read_csv(csv_path)
+
+        chunk_embeddings_df["embedding"] = chunk_embeddings_df["embedding"].apply(lambda x: np.fromstring(x.strip("[]"), sep=" "))
+
+        embeddings = torch.tensor(np.stack(chunk_embeddings_df["embedding"].tolist(), axis=0), dtype=torch.float32).to(device)
+
+        text_chunks = chunk_embeddings_df.to_dict(orient="records")
+
+        embedding_model = SentenceTransformer(model_name_or_path="all-mpnet-base-v2", device=device)
+
+        indices = retrieve_context(query=cveDesp, embeddings=embeddings, model=embedding_model)
+
+        context_items = [text_chunks[i] for i in indices]
+
+        context_str = "- " + "\n- ".join([item["sentence_chunk"] for item in context_items])
 
 
-    text_chunks_and_embedding_df["embedding"] = text_chunks_and_embedding_df["embedding"].apply(lambda x: np.fromstring(x.strip("[]"), sep=" "))
+        messages = [
+            {"role": "system", "content": f""""You are a Q&A Assistant. You will be provided with relevant information about various CVEs. Based on this information, your task is to recommend the CVE number that most closely matches the description of the vulnerability.
+            Provided Relevant Information: {context_str} 
+            """},
+            {"role": "user", "content": f""" Hello, could you recomend me a CVE that most closly resembles this chunk of text based of the relevant information that you have. Chunk of text: {cveDesp}"""},
+            ]   
 
-    embeddings = torch.tensor(np.stack(text_chunks_and_embedding_df["embedding"].tolist(), axis=0), dtype=torch.float32).to(device)
-
-    pages_and_chunks = text_chunks_and_embedding_df.to_dict(orient="records")
-
-    # Create model
-    from sentence_transformers import util, SentenceTransformer
-
-    embedding_model = SentenceTransformer(model_name_or_path="all-mpnet-base-v2",
-                                      device=device)
-
+        ############
+        input_ids = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(model.device)
+        outputs = model.generate(input_ids, max_new_tokens=700, eos_token_id=tokenizer.eos_token_id, do_sample=True, temperature=0.3, top_p=0.9)
+        response = outputs[0][input_ids.shape[-1]:]
+        llamaSug = tokenizer.decode(response, skip_special_tokens=True)
     
-    from time import perf_counter as timer
-
-   
-
-    device = "cuda"
-    
-    
+        return llamaSug
 
 
-
-    def retrieve(query: str,
+def retrieve_context(query: str,
                 embeddings: torch.tensor,
-                model: SentenceTransformer=embedding_model,
-                n_resources_to_return: int=5,
-                print_time: bool=True):
-        """
-        Embeds a query with model and returns top k scores and indices from embeddings.
-        """
+                model: SentenceTransformer,
+                n_resources_to_return: int=5):
+    """
+    Embeds a query with model and returns top k scores and indices from embeddings.
+    """
 
-        # Embed the query
-        query_embedding = model.encode(query, convert_to_tensor=True)
+    # Embed the query
+    query_embedding = model.encode(query, convert_to_tensor=True)
 
  
-        dot_scores = util.dot_score(query_embedding, embeddings)[0]
+    dot_scores = util.dot_score(query_embedding, embeddings)[0]
 
-        scores, indices = torch.topk(input=dot_scores,
-                                 k=n_resources_to_return)
+    _, indices = torch.topk(input=dot_scores, k=n_resources_to_return)
 
-        return scores, indices
+    return indices
 
-
-    from transformers import AutoTokenizer, AutoModelForCausalLM
-    import torch
-
-    model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
-
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    llm_model = AutoModelForCausalLM.from_pretrained(
-        model_id,
-        torch_dtype=torch.bfloat16,
-        #torch_dtype=torch.float32,
-        device_map="auto",
-        #device_map="cpu",
-    )
-
-    companyNameQuery = cveDesp
-    user_questions = [companyNameQuery,]
-
-
-    query_list = user_questions
-
-    import random
-
-    query = random.choice(query_list)
-
-    # Get just the scores and indices of top related results
-    scores, indices = retrieve(query=query,
-                                              embeddings=embeddings)
-
-
-    query = random.choice(query_list)
-
-    # Get relevant resources
-    scores, indices = retrieve(query=query,
-                                              embeddings=embeddings)
-
-    # Create a list of context items
-    context_items = [pages_and_chunks[i] for i in indices]
-
-
-    #print(prompt)
- 
-    def prompt_Context(query: str,
-                         context_items: list[dict]) -> str:
-        context = "- " + "\n- ".join([item["sentence_chunk"] for item in context_items])
-        return context
-
-
-    theContext= prompt_Context(query=query,
-                          context_items=context_items)
-
-
-    #print(theContext)
-
-    ##print(cveDesp)
-
-
-
-    messages = [
-        {"role": "system", "content": f""""You are a Q&A Assistant. You will be provided with relevant information about various CVEs. Based on this information, your task is to recommend the CVE number that most closely matches the description of the vulnerability.
-        Provided Relevant Information: {theContext} 
-    """},
-        {"role": "user", "content": f""" Hello, could you recomend me a CVE that most closly resembles this chunk of text based of the relevant information that you have. Chunk of text: {cveDesp}"""},
-        #{"role": "user", "content": f""" Can you give me a Indicators of Compromise (IoC) Report of Palo Alto Networks given the context; you must provide relevant CVE numbers associated with each threat. Please ignore the CVE's that are note related to the company. The context is the following: {theContext}"""},
-    ]   
-
-    ############
-    input_ids = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(model.device)
-    outputs = model.generate(input_ids, max_new_tokens=700, eos_token_id=tokenizer.eos_token_id, do_sample=True, temperature=0.3, top_p=0.9)
-    response = outputs[0][input_ids.shape[-1]:]
-    llamaSug = tokenizer.decode(response, skip_special_tokens=True)
     
-    return llamaSug
 
 
 
@@ -234,7 +165,7 @@ for cve in cves:
     try:
         first_set, second_set = extract_cve_numbers(cve)
         formatted_x = format_second_set(second_set)
-        file_path = f"cvelistV5/cves/{first_set}/{formatted_x}/CVE-{first_set}-{second_set}.json"
+        file_path = f"../cvelistV5/cves/{first_set}/{formatted_x}/CVE-{first_set}-{second_set}.json"
 
         with open(file_path, 'r') as f:
             data = json.load(f)
@@ -257,43 +188,14 @@ for cve in cves:
         
         torch.cuda.empty_cache()
 
-        #model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
-
-        #tokenizer = AutoTokenizer.from_pretrained(model_id)
-        #model = AutoModelForCausalLM.from_pretrained(
-        #model_id,
-        #torch_dtype=torch.bfloat16,
-        #device_map="auto",
-        #)
-
         messages = [
             {"role": "system", "content": "You are a chat bot."},
-            {"role": "user", "content": f"""Here is a list of CVEs: {cves}. I will provide you with a report shortly. If you encounter any CVEs not included in the list I previously provided, please describe each of them in two sentences. The provided text is as follows: {all_text}.
+            {"role": "user", "content": f"""In 2 sentences, could you describe the use of {cve} in the provided text. Provided text: {all_text}.
 """},
             ]
+        
 
-        #input_ids = tokenizer.apply_chat_template(
-        #messages,
-        #add_generation_prompt=True,
-        #return_tensors="pt"
-        #).to(model.device)
-
-       # terminators = [
-        #    tokenizer.eos_token_id,
-        #    tokenizer.convert_tokens_to_ids("<|eot_id|>")
-        #    ]
-
-       # outputs = model.generate(
-         #   input_ids,
-          #  max_new_tokens=500,
-           # eos_token_id=terminators,
-           # do_sample=True,
-          #  temperature=0.3,
-         #   top_p=0.9,
-        #    )
-        #response = outputs[0][input_ids.shape[-1]:]
-        #print(tokenizer.decode(response, skip_special_tokens=True))
-        #llamaDesc = tokenizer.decode(response, skip_special_tokens=True)
+        
 
         ############
         input_ids = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(model.device)
@@ -306,12 +208,10 @@ for cve in cves:
 
 
         ##########
+        llamaSug = asking_llama_for_advice(llamaDesc)
 
-        ##print(f"Hello ladies and gentleman, the llama has spoken, the output is {llamaDesc}")
 
         
-
-        llamaSug = askingLlamaForAdvice(llamaDesc)
 
 # Menu options
 def menu_option_1(all_text, tokenizer, model):
